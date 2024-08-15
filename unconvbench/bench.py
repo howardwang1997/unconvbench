3b33649d5d80092071311f4eda6c85cc504f8527
howardwang1997 <howardwang1997@gmail.com>
2024-08-15 11:48:24 +0800

    add example script

--- unconvbench/bench.py
+++ unconvbench/bench.py
@@ -8,7 +8,7 @@ from .constant import PRESET_MAPPER
 
 
 class UnconvbenchBenchmark(MSONable):
-    def __init__(self, **kwargs):
+    def __init__(self, autoload=False, **kwargs):
         dt = DatasetsTasks()
         self.DATASETS_TASKS = dt.get_tasks()
         for k in self.DATASETS_TASKS.keys():
@@ -16,13 +16,15 @@ class UnconvbenchBenchmark(MSONable):
 
         self.all_tasks = self.DATASETS_TASKS.values()
         self.user_metadata = None
+        if autoload:
+            self.load()
 
     @property
     def tasks(self):
         return self.all_tasks
 
     def from_preset(self, subset_type, **kwargs):
-        assert subset_type in PRESET_MAPPER.keys()
+        assert subset_type in PRESET_MAPPER.keys(), f'Only subset_type within {list(PRESET_MAPPER.keys())} supported!'
         self.all_tasks = {k: v for k, v in self.DATASETS_TASKS.items() if k in PRESET_MAPPER[subset_type]}
         return self
 
